f5e7351b41d60a7aaf0ee143db83c338e7ba3e54
goonhope <50105128+goonhope@users.noreply.github.com>
2020-04-30 17:48:37 +0800

    Update cpc_appendix.bat

--- cpc_appendix.bat
+++ cpc_appendix.bat
@@ -1,5 +1,5 @@
 @echo off
-echo 需安装 ImageMagick & ghostscript, 放到pdf文件目录运行
+echo 需安装 ImageMagick & ghostscript；本文件放到pdf文件目录运行
 for %%i in (*.pdf) do magick convert -density 300 -quality 100 %%i %%~ni_%03d.jpg
 echo sleep 5s
 ping 8.8.8.8 -n 10 > nul
