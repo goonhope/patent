018732dd18cb784997bcfa1b54aa6150b8603c51
goonhope <50105128+goonhope@users.noreply.github.com>
2020-04-30 16:44:18 +0800

    Update cpc_appendix.bat

--- cpc_appendix.bat
+++ cpc_appendix.bat
@@ -1,8 +1,7 @@
 @echo off
-echo 需安装ImagicMagick & ghostscript,
-md new
+echo 需安装ImagicMagick & ghostscript
 for %%i in (*.pdf) do magick convert -density 300 -quality 100 %%i %%~ni_%03d.jpg
 echo sleep 5
-ping 8.8.8.8 -n 10
+ping 8.8.8.8 -n 10 > nul
 if not exist done md done 
-for %%i in (*.jpg) do magick convert -resize 25% %%i done\%%~ni.jpg
+for %%i in (*.jpg) do magick convert -resize 25% %%i done\%%i
