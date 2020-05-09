28107163027b3de699d1010a4fae124cbf0bc0a8
goonhope <50105128+goonhope@users.noreply.github.com>
2020-05-09 22:26:15 +0800

    Update cpc_img2pdf.py

--- cpc_img2pdf.py
+++ cpc_img2pdf.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
-import os, re, shutil, cchardet, time
+import os, re, shutil, cchardet, time, img2pdf, zipfile,xlwt
 from lxml import etree as et
-import img2pdf,zipfile
 
 '''
 @FileName	:   cpc_img2pdf.py
 @Created  :   Oct,2018
+@Created  :   May,2020
 @Author		:   goonhope@gmail.com
-@Function	:   专利通知书批量重命名，转pdf文件—
-                        CPC直接导出通知文件解压，读取list.xml 
-                        信息在批量重命名，删除其它文件
+@Function	:   专利申请中间文件批量归档，xml to list
 '''
-
 
 def file_encoding(file_in):
     '''文件编码查询'''
@@ -20,7 +17,7 @@ def file_encoding(file_in):
     return cchardet.detect(data)['encoding']
 
 
-def read_xml(xml_f,encod="UTF-8"):
+def read_xml(xml_f, encod="GBK"):
     '''读取xml文件待改进'''
     parser = et.XMLParser(encoding=encod, load_dtd=True)  # list GBK other
     root = et.parse(xml_f, parser).getroot()
@@ -32,6 +29,50 @@ def read_xml(xml_f,encod="UTF-8"):
     all_dict["WENJIANMC"] = [x.text for x in root.cssselect("WENJIANMC")]
     # all_text ={x:y.strip() for x,y in all_text.items() if len(y) >= 3}
     return all_dict
+
+
+def tiftopdf(location):
+    '''批量转成pdf文件'''
+    newhold = ["_".join(x.split("_")[:-1]) for x in os.listdir(location) if x.endswith(".tif")]
+    newhold_unqe = list(set(newhold))
+    for x in newhold_unqe:
+        new = [y for y in os.listdir(location) if "_".join(y.split("_")[:-1]) == x ]
+        img_to_pdf(location, new)
+    print("已生成{}个pdf文件".format(len(newhold_unqe)))
+
+
+def img_to_pdf(path, images):
+    # 图片转pdf 默认布局
+    # a4inpt = (img2pdf.mm_to_pt(210), img2pdf.mm_to_pt(297))
+    # layout_fun = img2pdf.get_layout_fun(a4inpt)
+    images = [os.path.join(path,x) for x in images]
+    with open(os.path.splitext(images[0])[0][:-3] + '.pdf', "wb") as f:
+        # f.write(img2pdf.convert(images,layout_fun=layout_fun))
+        f.write(img2pdf.convert(images))
+
+
+def timer(func):
+    '''默认计时装饰器'''
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        print("处理中...")
+        location, n = func(*args, **kwargs)
+        print(f"@{func.__name__}:\t[Time:{time.time() - start : 0.3f}s]\t[归档数量:{n}条]，清单如下：")
+        print("\n".join(os.listdir(location)))
+        return
+    return wrapper
+
+
+def copy_req_dirs(source,location):
+    """复制符合要求的文件夹，后期可能需要修改"""
+    dirs = os.listdir(source)
+    for sor in dirs:
+        fsor = os.path.join(source,sor)
+        if os.path.isdir(fsor):
+            lsor = os.path.join(location,sor)
+            if sor.startswith("GA") or sor.startswith("14"):
+                shutil.copytree(fsor,lsor)
+    time.sleep(1)
 
 
 def xf(path,delta=True):
@@ -61,123 +102,99 @@ def xf(path,delta=True):
                         olddir = os.path.join(root, dir)
                         newnamedir = os.path.join(root, dir.encode('cp437').decode("GBK"))
                         os.renames(olddir, newnamedir)   # 修正编码
-            os.remove(os.path.join(path, rar))  if delta else None  # 删除rar
-
-
-def rename(location):
-    '''专利通知书图片重命名，并移动到根目录——单压缩包.'''
-    dirs = [x for x in os.listdir(location) if "GA" in x]
-    n = 1
-    #  获取专利l信息list
-    for dir in dirs:
-        xml_name = os.path.join(location, f"{dir}\\list.xml")
-        enc = file_encoding(xml_name)  # 获取文件编码
-        with open(xml_name,"r", encoding=enc) as fi:
-            f = fi.read()
-            info = re.findall(r"<TONGZHISID>(.+)<", f) + re.findall(r"<SHENQINGH>(.+)<", f) \
-                   + re.findall(r"<FAMINGMC>(.+)<", f) + re.findall(r"<TONGZHISMC>(.+)<", f) \
-                   + re.findall(r"<WENJIANMC>(.+)<", f)
-                   # + re.findall(r"<FAWENXLH>(.+)<", f)
-                   #通知书ID、申请号、发明名称、通知书名称、授权通知书压缩包、发文序列号
-            xf(os.path.join(location, f'{info[0]}\\' * 2)) if info[-1].endswith("zip") else None
-        print(*info)
-        # 重命名后移动到根目录——路径规律
-        info_short = [x.strip(".zip") for x in info if "GA" in x or x.endswith("zip")]
-        names_hold = ["", "授权通知书", "检索报告"]
-        for info_in, name_mid  in zip(info_short,names_hold):
-            imgdir = os.path.join(location, f'{info[0]}\\' * 2 + info_in)
-            imgfiles = [x for x in os.listdir(imgdir)]
-            for name in imgfiles:
-                old = os.path.join(imgdir, name)
-                num = 3 if name_mid else 4
-                mid = f"_{name_mid}_" if name_mid else "_"
-                newname = os.path.join(imgdir, "_".join(info[:num]) + mid + name[4:])
-                os.renames(old, newname)
-                n += 1
-                shutil.move(newname, location)
-    return location,n
-
-
-def tiftopdf(location):
-    '''批量转成pdf文件'''
-    newhold = ["_".join(x.split("_")[:-1]) for x in os.listdir(location) if x.endswith(".tif")]
-    newhold_unqe = list(set(newhold))
-    for x in newhold_unqe:
-        new = [y for y in os.listdir(location) if "_".join(y.split("_")[:-1]) == x ]
-        img_to_pdf(location, new)
-    print("已生成{}个pdf文件".format(len(newhold_unqe)))
-
-
-def img_to_pdf(path, images):
-    # 图片转pdf 默认布局
-    # a4inpt = (img2pdf.mm_to_pt(210), img2pdf.mm_to_pt(297))
-    # layout_fun = img2pdf.get_layout_fun(a4inpt)
-    images = [os.path.join(path,x) for x in images]
-    with open(os.path.splitext(images[0])[0][:-3] + '.pdf', "wb") as f:
-        # f.write(img2pdf.convert(images,layout_fun=layout_fun))
-        f.write(img2pdf.convert(images))
-
-
-def delete(location):
-    '''删除指定格式以外的文件和文件夹，显示文件名'''
-    [shutil.rmtree(os.path.join(location, x)) for x in os.listdir(location) \
-    if os.path.isdir(os.path.join(location, x)) ]
-    os.system(r"start " + location)
-    return None
-
-
-def timer():
-    '''默认计时装饰器'''
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        location, n = func(*args, **kwargs)
-        print(f"@{func.__name__}:\t[Time:{time.time() - start : 0.3f}s]\t[归档数量:{n}]")
-        print("\n".join(os.listdir(location)))
-        return
-    return wrapper
-
+            os.remove(os.path.join(path, rar)) if delta else None  # 删除rar
 
 
 @timer
-def appy(zipfile):
-    xf(zipfile)
-    time.sleep(1)
-    location = os.path.splitext(zipfile)[0]
-    n = rename(location)
-    delete(location)  # 删除非tif文件
-    tiftopdf(location)  # 转成pdf文件
-    # delete(location,"pdf")  # 删除非pdf文件
-    os.system("start %s" % location)
-    return location,n
-
-
-@timer
-def grant(zipfile,delta=False):
-    '''授权专利证书批量重命名'''
-    location = os.path.splitext(zipfile)[0]
-    xf(zipfile, delta)
-    time.sleep(1)
-    dirs = [x for x in os.listdir(location) if x.startswith("14")]
-    num = 0
+def filed(location, source=True):
+    '''授权申请过程文件及证书批量重命名，默认直接复制CPC notices 目录'''
+    if source:
+        source = r"C:\Program Files (x86)\gwssi\CPC客户端\cases\notices"
+        shutil.rmtree(location) if os.path.exists(location) else None  # 避免重复
+        os.makedirs(location) if not os.path.exists(location) else None  # 建文件
+        if os.path.exists(source):
+            copy_req_dirs(source, location)  # 复制
+        else:
+            print("未安装cpc，请直接导出解压到location目录")
+            return False
+    dirs = [x for x in os.listdir(location) if os.path.isdir(os.path.join(location,x))]
+    num = 0  # 计数
+    intxt = ["受理","合格","实质","专利证书","审查意见","办理登记"]
+    infos = [["发文序号","申请号","发明名称","通知书名称"],]
     for dir in dirs:
         xml_file = os.path.join(location, f"{dir}\\list.xml")
         if os.path.exists(xml_file):
-            info = read_xml(xml_file, "GBK")
-            if "专利证书" in info['TONGZHISMC']:
-                shortn_name = f"{info['SHENQINGH']}_{info['FAMINGMC']}_证书.pdf"
-                old_name = os.path.join(location,f"{dir}\\" + f"{dir[:-2]}\\" *2 + f"{dir[:-2]}.pdf")
-                shutil.move(old_name,location)
-                fn_name = os.path.join(location, f"{dir[:-2]}.pdf")
-                time.sleep(0.5) if os.path.exists(fn_name) else None
-                nw_name = os.path.join(location,shortn_name)
-                os.rename(fn_name,nw_name)
-                num += 1
-    [shutil.rmtree(os.path.join(location,x)) for x in dirs]
+            info = read_xml(xml_file)
+            notice_name = info['TONGZHISMC']  # 通知书名称
+            if any(x in notice_name for x in intxt):
+                notice_id = info['TONGZHISID']  # 通知书id
+                appdx = info["WENJIANMC"]  # 附件
+                app_id = info['SHENQINGH']  # 申请号
+                invt_name = info['FAMINGMC']  # 发明名称
+                infos.append(["'" + notice_id,"'" + app_id,invt_name,notice_name])
+                sub_dir = os.path.join(location, f"{dir}\\" + f"{notice_id}\\" * 2) # 直接根目录
+                sub_files = get_files(sub_dir)
+                for sub_f in sub_files:
+                    old_file = os.path.join(sub_dir, sub_f)
+                    appendix = ".pdf" if "证书" in notice_name else f"_{sub_f[-6:]}"
+                    new_sub_f = f"{app_id}_{invt_name}_{notice_name}" + appendix
+                    new_file = os.path.join(location, new_sub_f)
+                    os.renames(old_file,new_file)
+                    time.sleep(1)
+                    num += 1
+                condtion = any(x in notice_name for x in intxt[-2:]) if appdx else False
+                if condtion:  # 附件
+                    sub_dir = os.path.join(location, f"{dir}\\{notice_id}")
+                    for nu,wjc in enumerate(appdx):
+                        wjc_zip = os.path.join(sub_dir, wjc)
+                        wjc_dir = os.path.splitext(wjc_zip)[0]
+                        xf(wjc_zip) if not os.path.exists(wjc_dir) else None  # 解压zip
+                        time.sleep(1)
+                        sub_files = get_files(wjc_dir)
+                        for sub_f in sub_files:
+                            old_file = os.path.join(wjc_dir, sub_f)
+                            mid = f"_授权通知书_{nu}" if intxt[-1] in notice_name else f"检索or对比_{nu}"
+                            new_sub_f = f"{app_id}_{invt_name}_{mid}_" + sub_f[-6:]
+                            new_file = os.path.join(location, new_sub_f)
+                            # 删除 授权通知书的检索文件
+                            os.renames(old_file, new_file) if not (nu > 0 and "授权通知书" in new_sub_f) else None
+                            time.sleep(1)
+                            num += 1
+    [shutil.rmtree(os.path.join(location, x)) for x in dirs]  # 删除目录txt
+    save_to_excel(infos, os.path.join(location,"list.xls"))
+    tiftopdf(location)  # 转成pdf文件
     os.system("start %s" % location)
     return location, num
 
 
+def get_files(path,ext=("tif","tiff","pdf")):
+    """获取pdf 、tif文件"""
+    files = [x for x in os.listdir(path) if x.split(".")[-1].lower() in ext]
+    return files
+
+
+def save_to_excel(info,file_out):
+    '''写入excel'''
+    file = xlwt.Workbook()
+    table = file.add_sheet('list', cell_overwrite_ok=True)
+    style = xlwt.XFStyle()
+    for i in range(len(info)):
+        for j in range(len(info[i])):
+            table.write(i, j, info[i][j], style)
+    file.save(file_out)
+
+
+def main():
+    path = input("输入归档或压缩包目录： ").strip()
+    if path.endswith(".zip") and os.path.exists(path):
+        xf(path,False)  # zip 解压
+        path = os.path.splitext(path)[0]
+        filed(path, False)
+    elif not os.path.exists(path):  # 新目录默认直接copy notices文件 到path文件
+        filed(path)
+    else:
+        filed(path, False)  # 解压目录
+
+
 if __name__ == '__main__':
-    zipfile = input("输入导出zip文件路径：").strip()
-    # appy(zipfile)  # 申请文件归档
-    grant(zipfile) # 证书文件归档
+    main()
