f236dfe29a52cb5dc2650d138e69989518f3ce86
goonhope <50105128+goonhope@users.noreply.github.com>
2020-05-17 13:58:11 +0800

    Update cpc_appendix.bat

--- cpc_appendix.bat
+++ cpc_appendix.bat
@@ -4,7 +4,7 @@ for %%i in (*.pdf) do magick convert -density 300 -quality 100 %%i %%~ni_%03d.jp
 echo sleep 5s && timeout /t 5 /nobreak >nul
 ::ping -n 10 127.0.0.1 > nul or choice /N /C y /T 5 /D y >nul
 ::获取缩放比例
-for /f %%i in (*.jpg) do (for /f "tokens=3 delims=x " %%j in ('magick identify 0.jpg') do set /a scale=620*100/%%j)
+for /f "tokens=3 delims=x " %%i in ('magick identify *.jpg') do set /a scale=620*100/%%i && cls
 if not exist done md done 
 ::for %%i in (*.jpg) do magick convert -resize 25% %%i done\%%i
 for %%i in (*.jpg) do magick convert -resize %scale%% %%i done\%%~nxi
