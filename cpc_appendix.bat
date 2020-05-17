a03ffea3f1edde24c11adcd8c746711ceb9a493e
goonhope <50105128+goonhope@users.noreply.github.com>
2020-05-17 19:14:26 +0800

    Update cpc_appendix.bat

--- cpc_appendix.bat
+++ cpc_appendix.bat
@@ -7,4 +7,5 @@ echo sleep 5s && timeout /t 5 /nobreak >nul
 for /f "tokens=3 delims=x " %%i in ('magick identify *.jpg') do set /a scale=620*100/%%i && cls
 if not exist done md done 
 ::for %%i in (*.jpg) do magick convert -resize 25% %%i done\%%i
-for %%i in (*.jpg) do magick convert -resize %scale%% %%i done\%%~nxi
+for %%i in (*.jpg) do magick convert -resize %scale%% %%i done\%%i
+::or for %%i in (%%~dp0*.jpg) do magick convert -resize %scale%% %%~fi %%~dp0done\%%~nxi
