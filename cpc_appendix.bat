3e2741e562c2ba9a5b1db99511d3a3242e3a677f
goonhope <50105128+goonhope@users.noreply.github.com>
2020-05-17 19:17:50 +0800

    Update cpc_appendix.bat

--- cpc_appendix.bat
+++ cpc_appendix.bat
@@ -1,5 +1,5 @@
 @echo off
-echo 需安装 ImageMagick & ghostscript；本文件放到pdf文件目录运行(a4)
+echo 需安装 ImageMagick & ghostscript,并加入path；本文件放到pdf文件目录运行(a4)
 for %%i in (*.pdf) do magick convert -density 300 -quality 100 %%i %%~ni_%03d.jpg
 echo sleep 5s && timeout /t 5 /nobreak >nul
 ::ping -n 10 127.0.0.1 > nul or choice /N /C y /T 5 /D y >nul
@@ -8,4 +8,4 @@ for /f "tokens=3 delims=x " %%i in ('magick identify *.jpg') do set /a scale=620
 if not exist done md done 
 ::for %%i in (*.jpg) do magick convert -resize 25% %%i done\%%i
 for %%i in (*.jpg) do magick convert -resize %scale%% %%i done\%%i
-::or for %%i in (%%~dp0*.jpg) do magick convert -resize %scale%% %%~fi %%~dp0done\%%~nxi
+::for %%i in (%%~dp0*.jpg) do magick convert -resize %scale%% %%~fi %%~dp0done\%%~nxi
