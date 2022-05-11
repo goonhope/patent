8ab0391bae771d927ed2eb554a820bcb6b2f5319
goonhope <50105128+goonhope@users.noreply.github.com>
2022-05-11 11:26:58 +0800

    Add files via upload

---
+++ cpc_updated.bat
@@ -0,0 +1,20 @@
+@echo off
+title       =========CPC一键升级（直接）=========
+echo     提示：文件需拷贝到更新包目录并管理员运行
+echo    ==========goonhope@qq.com==========
+
+::获取目录
+set root=%~dp0
+set cpc="C:\Program Files (x86)\gwssi\CPC客户端\"
+
+::赋权更新
+cacls %cpc% /e /g users:f
+attrib %cpc%* -s -h -r /s
+::rd /s /q "%cpc%Conversion"
+cd %root%
+call OffLineUpdate.exe
+
+::注册组件
+cd %cpc%
+for %%i in (*.ocx gwssiSys.dll) do regsvr32 /s %%i
+pause
